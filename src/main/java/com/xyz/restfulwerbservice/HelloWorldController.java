a3e0cd69ca9bdf87f360987aa34f15c52874ff44
2songyi <5469011s2@daum.net>
2022-10-07 13:00:07 +0900

    first commit

---
+++ src/main/java/com/xyz/restfulwerbservice/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.xyz.restfulwerbservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	// GET
+	// hello-world 를 url로 설정 -> endpoint
+	
+	// @RequestMapping()
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		// http://localhost:8088/hello-world
+		return "Hello World";
+	}
+	
+	@GetMapping("/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping("/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s",name));
+	}
+}
---
+++ src/main/java/com/xyz/restfulwerbservice/RestfulWebServiceApplication.java
@@ -0,0 +1,14 @@
+package com.xyz.restfulwerbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServiceApplication.class, args);
+		// run안에 실행하고자 하는 클래스 이름을 적음
+	}
+
+}
